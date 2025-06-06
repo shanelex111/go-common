726770b998c31ec47f99fafc600d9193538a5748
ShaneLex <shanelex111@gmail.com>
2025-06-06 17:39:56 +0800

    update config 2.

--- pkg/config/service.go
+++ pkg/config/service.go
@@ -2,12 +2,12 @@ package config
 
 import "github.com/spf13/viper"
 
-func Load(path, name string) (err, *viper.Viper) {
+func Load(path, name string) (*viper.Viper, error) {
 	v := viper.New()
 	v.SetConfigName(name)
 	v.AddConfigPath(path)
 	if err := v.ReadInConfig(); err != nil {
-		return err, nil
+		return nil, err
 	}
-	return nil, v
+	return v, nil
 }
