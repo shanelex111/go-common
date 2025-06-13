de14d2510e88567f9d01eef7328b52ee53e3ca66
xinyu.zhang <zhang.li@net263.com>
2025-06-13 17:23:07 +0800

    add engine.

---
+++ pkg/engine/config.go
@@ -0,0 +1,56 @@
+package engine
+
+import (
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	defaultKey = "engine"
+
+	ModeDev     = "dev"
+	ModeTest    = "test"
+	ModePreProd = "pre-prod"
+	ModeRelease = "release"
+)
+
+var (
+	cfg *config
+)
+
+type config struct {
+	Port string `mapstructure:"port"`
+	Mode string `mapstructure:"mode"`
+}
+
+func Init(v *viper.Viper) {
+	initConfig(v)
+}
+
+func initConfig(v *viper.Viper) {
+	cfg = &config{}
+	if err := v.Sub(defaultKey).Unmarshal(cfg); err != nil {
+		panic(err)
+	}
+
+}
+
+func GetMode() string {
+	switch strings.ToLower(cfg.Mode) {
+	case ModeDev:
+		return ModeDev
+	case ModeTest:
+		return ModeTest
+	case ModePreProd:
+		return ModePreProd
+	case ModeRelease:
+		return ModeRelease
+	default:
+		return ModeDev
+	}
+}
+
+func GetPort() string {
+	return cfg.Port
+}
