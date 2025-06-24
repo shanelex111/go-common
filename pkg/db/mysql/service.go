70ebe355fe229171e69c22c3185fff5c8ace8def
xinyu.zhang <zhang.li@net263.com>
2025-06-24 13:05:46 +0800

    update mysql.

--- pkg/db/mysql/service.go
+++ pkg/db/mysql/service.go
@@ -15,9 +15,9 @@ func (gl *GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (str
 	elapsed := time.Since(begin)
 	sql, rows := fc()
 	entry := logrus.WithFields(logrus.Fields{
-		"elapsed": elapsed,
-		"rows":    rows,
-		"sql":     sql,
+		"mysql.elapsed": elapsed.Milliseconds(),
+		"mysql.rows":    rows,
+		"mysql.sql":     sql,
 	})
 	switch {
 	case err != nil && gl.LogLevel >= logger.Error:
